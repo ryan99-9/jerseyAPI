84002539d2582d7ecd3d8c3e1e95bedd785243d4
unknown <edoafrian90@gmail.com>
2022-08-22 15:05:03 +0700

    database

---
+++ database/migrations/2022_08_22_071536_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',50);
+            $table->string('brand',50);
+            $table->unsignedBigInteger('price',50);
+            $table->unsignedBigInteger('stock',50);
+            $table->unsignedBigInteger('wishlist');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
